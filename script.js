2f0bca53761c74999947f6336e22259c2668bdda
soph0610 <azrooh0610@gmail.com>
2019-05-03 11:20:19 +0900

    사진첩 수정

--- script.js
+++ script.js
@@ -81,6 +81,7 @@ function turnOnEmoji(id) {
   }
 }
 
+//image slider
 let slideIndex = 1;
 showDivs(slideIndex);
 
@@ -95,10 +96,26 @@ function currentDiv(n) {
 function showDivs(n) {
   let i;
   const x = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+}
+
+/*function showDivs(n) {
+  let i;
+  const x = document.getElementsByClassName("mySlides");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   x[slideIndex-1].style.display = "block";  
-}
+}*/

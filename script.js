54a93de91c34c17a776f91523582e65bfd11da08
soph0610 <azrooh0610@gmail.com>
2019-05-04 12:56:01 +0900

    지도 수정

--- script.js
+++ script.js
@@ -30,21 +30,21 @@ window.onload = function () {
   const container = document.getElementById('map');
   const options = {
     center: new daum.maps.LatLng(37.48696198627081, 127.03344609502346), //지도의 중심좌표.
-	  level: 3 //지도의 레벨(확대, 축소 정도)
+	  level: 4 //지도의 레벨(확대, 축소 정도)
   };
 
   const map = new daum.maps.Map(container, options); //맵 생성
 
-  const imageSrc = '/assets/img/marker.png', // 마커이미지의 주소입니다    
+  const imageSrc = 'assets/img/marker.png', // 마커이미지의 주소입니다    
         imageSize = new daum.maps.Size(64, 69), // 마커이미지의 크기입니다
         imageOption = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
         
   // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-  var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize, imageOption),
+  const markerImage = new daum.maps.MarkerImage(imageSrc, imageSize, imageOption),
       markerPosition = new daum.maps.LatLng(37.48696198627081, 127.03344609502346); // 마커가 표시될 위치입니다
 
   // 마커를 생성합니다
-  var marker = new daum.maps.Marker({
+  const marker = new daum.maps.Marker({
       position: markerPosition, 
       image: markerImage // 마커이미지 설정 
   });
@@ -52,8 +52,8 @@ window.onload = function () {
   // 마커가 지도 위에 표시되도록 설정합니다
   marker.setMap(map); 
 
-  const iwContent = '<div style="padding:5px;">브라이드밸리', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-        iwPosition = new daum.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+  const iwContent = '<div style="padding:2px;">캠코양재타워 브라이드밸리</div>',
+        iwPosition = new daum.maps.LatLng(37.48696198627081, 127.03344609502346); //인포윈도우 표시 위치입니다
 
   // 인포윈도우를 생성합니다
   const infowindow = new daum.maps.InfoWindow({
@@ -67,6 +67,7 @@ window.onload = function () {
   // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
   const zoomControl = new daum.maps.ZoomControl();
   map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
+
 }
 
 function turnOnEmoji(id) {
@@ -98,4 +99,16 @@ const span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
+}
+
+const copyimg = document.getElementById('copy');
+copyimg.onclick = function() {
+  const address = "서울 강남구 강남대로 262 캠코양재타워 B1층";
+  const el = document.createElement('textarea');
+  el.value = address;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  alert("복사 완료! 조금 이따 봐요! :D ")
 }
